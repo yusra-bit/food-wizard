e7871194088a201df077bc2cfeb5a553c62d1807
Yusra Abdi <yusra4411@gmail.com>
2023-08-13 15:23:11 +0300

    create a responsive🧙 navbar

---
+++ my-app/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import './Navbar.css';
+
+import {Link, NavLink} from "react-router-dom";
+
+export const Navbar = () => {
+    const [menuOpen, SetMenuOpen] = useState(false)
+    return (
+    <nav>  
+
+        <Link to="/" className="title">Food Wizard🍜🧙</Link>
+        <div className="menu" 
+        onClick={() => {
+            SetMenuOpen(!menuOpen);
+        }}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : "" }>
+        <li><NavLink to="/liked">Saved Recipes!</NavLink> </li>
+        <li><NavLink to="/about">About</NavLink> </li>
+        </ul>
+    </nav>
+    );
+}
